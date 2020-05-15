1d43b8a4cc671c4c0ec0a92090586ff8b52f34a5
mkilmer <marcoskilmer1@gmail.com>
2020-05-15 20:09:56 -0300

    Initial Commit

---
+++ IOS UITesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS UITesting
+//
+//  Created by Marcos Kilmer on 15/05/20.
+//  Copyright © 2020 mkilmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOS UITestingTests/IOS_UITestingTests.swift
@@ -0,0 +1,34 @@
+//
+//  IOS_UITestingTests.swift
+//  IOS UITestingTests
+//
+//  Created by Marcos Kilmer on 15/05/20.
+//  Copyright © 2020 mkilmer. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_UITesting
+
+class IOS_UITestingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
